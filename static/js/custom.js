9a58402eb0c4ac1036a31f2cbbf78d0a02d90493
Ken <kwill1253@gmail.com>
2021-01-22 15:03:58 -0500

    updates

---
+++ static/js/custom.js
@@ -0,0 +1,32 @@
+
+var sayHello = function () {
+    alert("Hello users");
+}
+
+var saySomething = function (val) {
+    alert(val);
+}
+
+var getUserEmail = function () {
+    var rtn = "";
+    var cname = "email";
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            rtn = c.substring(name.length, c.length);
+        }
+    }
+    return rtn;
+}
+
+var setUserEmail = function(val){
+    var d = new Date();
+    d.setTime(d.getTime() + (365 * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = "email" + "=" + val + ";" + expires + ";path=/";
+}
